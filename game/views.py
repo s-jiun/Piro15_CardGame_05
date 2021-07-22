42cab469c38250a883a999f5f029ef461fbc29a9
woong7 <rjsdnd980316@naver.com>
2021-07-22 22:37:09 +0900

    resolve conflict

--- game/views.py
+++ game/views.py
@@ -1,9 +1,11 @@
 
 from django.shortcuts import render
 from .models import CardGame, User
+from django.views import View
 from . import forms
 from django.contrib.auth import authenticate, login, logout
-from django.views import View
+
+
 
 # Create your views here.
 def game_info(request, pk):
